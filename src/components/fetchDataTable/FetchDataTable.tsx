ead22eb74e14156afebfa3a2701101aad9bf1f72
Almiñana Calderon <enric.alminanacalderon@capgemini.com>
2024-03-20 11:51:20 +0100

    ADD dummyjson api

--- src/components/fetchDataTable/FetchDataTable.tsx
+++ src/components/fetchDataTable/FetchDataTable.tsx
@@ -1,25 +1,27 @@
 import React, { useEffect, useState } from "react";
+import './FetchDataTable.css';
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const BASE_URL_DUMMY = 'https://dummyjson.com';
 
 export interface Post {
     id: number;
     title: string;
+    category: string;
 }
 
 export default function FetchDataTable() {
-    const [allPosts, setAllPosts] = useState<Post[]>([]);
+    const [allPosts, setAllPosts] = useState<Post[] | undefined>();
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<any>();
+    const [itemsNumber, setItemsNumber] = useState<number>(10);
 
     useEffect(() => {
-
         setIsLoading(true)
         const getPosts = async () => {
             try {
-                const response = await fetch(`${BASE_URL}/posts`);
-                const posts = await response.json() as Post[];            
-                setAllPosts(posts);
+                const responseDummy = await fetch(`${BASE_URL_DUMMY}/products?limit=${itemsNumber}`);
+                const posts = await responseDummy.json();            
+                setAllPosts(posts.products);
             } catch (error) {
                 setError(error);
             } finally {
@@ -33,7 +35,7 @@ export default function FetchDataTable() {
         setTimeout(() => {
             setIsLoading(false);
         }, 1500);
-    }, []);
+    }, [itemsNumber]);
 
     if (error) {
         return <div>Something went wrong. Try again please...</div>;
@@ -44,28 +46,31 @@ export default function FetchDataTable() {
             <h3>Fetch Data Test</h3>
             {isLoading && <div>Loading ...</div> }
             {!isLoading && (
-                // <div>
-                //     {allPosts.map((post) => {
-                //         return <div key={post.id}>{post.title}</div>
-                //     })}
-                // </div>
                 <div>
-                    <table>
+                    <table className="table">
                         <thead>
                             <tr>
-                                <td>ID</td>
-                                <td>Title</td>
+                                <td  className="productId">ID</td>
+                                <td className="productTitle">Title</td>
+                                <td>Price</td>
                             </tr>
                         </thead>
                         <tbody>
-                        {allPosts.map((post) => {
+                        {allPosts && allPosts.length > 0 && allPosts.map((post) => {
                             return <tr key={post.id}>
-                                <td >{post.id}</td>
+                                <td>{post.id}</td>
                                 <td>{post.title}</td>
+                                <td>{post.category}</td>
                             </tr>
                         })}
                         </tbody>
                     </table>
+                    <div className="pagination">
+                        <button>First</button>
+                        <button>Preview</button>
+                        <button>Next</button>
+                        <button>Last</button>
+                    </div>
                 </div>
             )}
         </div>
